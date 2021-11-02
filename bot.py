63ba2fd5c6ceb42f9ee55f153a8c03829dfb977a
Milo Price <milo.price@gmail.com>
2021-11-01 18:43:31 -0700

    Add support for message chain context
    Also adds reroll command, makes 'continue' more lenient, makes all
    messages by the bot replies, and allows interjection.

--- bot.py
+++ bot.py
@@ -7,15 +7,66 @@ from dotenv import load_dotenv
 
 import openai
 load_dotenv()
+
+MAX_DEPTH = 8
+
 TOKEN = os.getenv('DISCORD_TOKEN')
 OPEN_API_KEY = os.getenv('OPENAI_API_KEY')
 openai.api_key = OPEN_API_KEY
 engines = openai.Engine.list()
 
-
 message_queue = []
 
 # Based on https://realpython.com/how-to-make-a-discord-bot-python/
+
+def clean_text(message):
+    return message.clean_content.replace('@SmarterAdult ', '').strip()
+
+def invalid_continue(message):
+    return message.reference is None and clean_text(message).lower() in ['continue', 'go on']
+
+def invalid_reroll(message):
+    return message.reference is None and clean_text(message).lower() in ['reroll', 'try again']
+
+def should_continue(message):
+    return message.reference is not None and clean_text(message).lower() in ['continue', 'go on']
+
+def should_reroll(message):
+    return message.reference is not None and clean_text(message).lower() in ['reroll', 'try again']
+
+# TODO: should have graceful handling for the message not being found
+async def get_thread_text(message, depth=0):
+    if message.reference is None or depth >= MAX_DEPTH:
+        return clean_text(message)
+    # TODO: probably doesn't have to be a special case
+    elif message.reference and should_continue(message):
+        parent_id = message.reference.message_id
+        parent_message = await message.channel.fetch_message(parent_id)
+
+        return await get_thread_text(parent_message)
+    elif message.reference and should_reroll(message):
+        parent_id = message.reference.message_id
+        parent_message = await message.channel.fetch_message(parent_id)
+
+        # TODO: handle better
+        if not parent_message.reference:
+            raise "Cannot reroll"
+
+        grandparent_id = parent_message.reference.message_id
+        grandparent_message = await message.channel.fetch_message(grandparent_id)
+
+        return await get_thread_text(grandparent_message)
+    else:
+        parent_id = message.reference.message_id
+        parent_message = await message.channel.fetch_message(parent_id)
+
+        while should_continue(parent_message):
+            # The message is a user telling the bot to continue; get its parent instead
+            parent_id = parent_message.reference.message_id
+            parent_message = await message.channel.fetch_message(parent_id)
+
+        return await get_thread_text(parent_message, depth + 1) + ' ' + clean_text(message)
+
 
 client = discord.Client()
 
@@ -32,41 +83,32 @@ async def on_member_join(member):
 
 @client.event
 async def on_message(message):
-    # message_queue.append('@' + message.author.display_name + ': ' + message.clean_content)
-    # if len(message_queue) > 10:
-    #     message_queue.pop(0)
-    # print("message queue:")
-    # print(message_queue)
-
+    # Don't respond to self
     if message.author == client.user:
         return
+    # Don't respond unless mentioned
     if client.user not in message.mentions:
         return
-    # if message.channel.name != 'where-is-milo':
-    #     return
 
-    if message.reference is not None and message.clean_content.replace('@SmarterAdult ', '') == 'continue':
-        parent_id = message.reference.message_id
-        message = await message.channel.fetch_message(parent_id)
-        content = message.clean_content
-    else:
-        content = message.clean_content
-        content = content.replace('@SmarterAdult ', '')
+    # Help confused users
+    if invalid_continue(message) or invalid_reroll(message):
+        await message.reply("You need to reply to a message to do that!")
+        return
 
-    # content = "\n".join(message_queue) + '\n@SmarterAdult:'
+    content = await get_thread_text(message)
 
     print("content:")
     print(content)
 
-    # content = message.author.name + 'said to me, a very amusing comedian, ' + content + '. I replied:'
 
     completion = openai.Completion.create(engine="davinci", prompt=content, max_tokens = 64)
 
+    print("response:")
     print(completion.choices)
 
     response = completion.choices[0].text
 
-    await message.channel.send(response)
+    await message.reply(response)
 
 
 client.run(TOKEN)
